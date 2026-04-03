864be4ee0ff8a1f23d9ad1177da2844aed6923fb
lagaalfonso-a11y <267745594+lagaalfonso-a11y@users.noreply.github.com>
2026-04-03 17:31:29 -0300

    fix: explicit workspace root for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [],
   },
+  outputFileTracingRoot: path.resolve(__dirname),
 };
 
 module.exports = nextConfig;
